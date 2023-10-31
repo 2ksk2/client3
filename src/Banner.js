071df4fbe423df076f397860b56ce753ddc29fc2
wilypradana <pradanawily28@gmail.com>
2023-10-31 17:52:11 +0700

    first commit

---
+++ src/Banner.js
@@ -0,0 +1,15 @@
+"use client";
+
+import { Carousel } from "flowbite-react";
+
+export default function Banner() {
+  return (
+    <Carousel>
+      <img alt="..." src="https://source.unsplash.com/random/300x300/?city" />
+      <img alt="..." src="https://source.unsplash.com/random/300x300/?city" />
+      <img alt="..." src="https://source.unsplash.com/random/300x300/?city" />
+      <img alt="..." src="https://source.unsplash.com/random/300x300/?city" />
+      <img alt="..." src="https://source.unsplash.com/random/300x300/?city" />
+    </Carousel>
+  );
+}
---
+++ src/Navbar.js
@@ -0,0 +1,38 @@
+import "./App.css";
+
+function Navbar() {
+  return (
+    <>
+      <div className="max-w-2xl mx-auto">
+        <nav className="border-gray-200 px-2 mb-10  bg-blue-600 p-4 ">
+          <div className="container mx-auto flex flex-wrap items-center justify-between">
+            <div className="relative flex-1 mx-3 md:mx-0">
+              <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                <svg
+                  className="w-5 h-5 text-black font-extrabold"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </div>
+              <input
+                type="text"
+                id="email-adress-icon"
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2"
+                placeholder="Search..."
+              />
+            </div>
+          </div>
+        </nav>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
